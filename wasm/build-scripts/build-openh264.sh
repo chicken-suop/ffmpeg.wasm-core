3a2e477c33c3e63165aa5cc3a2c065100aa61434
Elliot Schep <elliot@desygner.com>
2022-02-07 17:26:55 +1000

    Revert "Add configure step for build openh264"
    This reverts commit d196a627d69380bc4e0207c9df720acfc5fccef3.

--- wasm/build-scripts/build-openh264.sh
+++ wasm/build-scripts/build-openh264.sh
@@ -5,9 +5,4 @@ source $(dirname $0)/var.sh
 
 LIB_PATH=third_party/openh264
 
-CONF_FLAGS=(
-  --prefix=$BUILD_DIR           # install library in a build directory for FFmpeg to include
-)
-echo "CONF_FLAGS=${CONF_FLAGS[@]}"
-(cd $LIB_PATH && emconfigure ./configure "${CONF_FLAGS[@]}")
 emmake make -C $LIB_PATH OS=linux ARCH=i686 ASM=yasm install-static

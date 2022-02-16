d77cd403a91d902cbd4f9a9a8d7a6cbf8b99e845
Elliot Schep <elliot@desygner.com>
2022-02-16 15:49:20 +1000

    Change openh264 build steps

--- wasm/build-scripts/build-openh264.sh
+++ wasm/build-scripts/build-openh264.sh
@@ -5,4 +5,5 @@ source $(dirname $0)/var.sh
 
 LIB_PATH=third_party/openh264
 
-emmake make -C $LIB_PATH OS=linux ARCH=i686 ASM=yasm install-static
+emmake make -C $LIB_PATH OS=linux ARCH=i686 ASM=yasm -j4
+emmake make install

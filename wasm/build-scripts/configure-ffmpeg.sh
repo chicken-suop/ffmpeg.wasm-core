f6f3ffc28ca2ef384a131daafbfa84ceefa22e1c
Elliot Schep <elliot@desygner.com>
2022-11-17 20:42:21 +0000

    add --disable-all

--- wasm/build-scripts/configure-ffmpeg.sh
+++ wasm/build-scripts/configure-ffmpeg.sh
@@ -5,6 +5,7 @@ source $(dirname $0)/var.sh
 
 FLAGS=(
   "${FFMPEG_CONFIG_FLAGS_BASE[@]}"
+  --disable-all # Do not remove
   --disable-network
   --disable-autodetect
   --disable-debug
@@ -17,21 +18,15 @@ FLAGS=(
   --enable-nonfree        # required by fdk-aac
   --enable-zlib           # enable zlib
   --enable-libx264        # enable x264
-  --enable-libx265        # enable x265
   --enable-libvpx         # enable libvpx / webm
   --enable-libwavpack     # enable libwavpack
-  --enable-libmp3lame     # enable libmp3lame
   --enable-libfdk-aac     # enable libfdk-aac
   --enable-libtheora      # enable libtheora
   --enable-libvorbis      # enable libvorbis
-  --enable-libfreetype    # enable freetype
   --enable-libopus        # enable opus
   --enable-libwebp        # enable libwebp
-  --enable-libass         # enable libass
-  --enable-libfribidi     # enable libfribidi
-  --enable-avdevice       # enable lavfi and anullsrc
 
-  --enable-ffmpeg
+  --enable-avdevice       # enable lavfi and anullsrc
   --enable-avcodec
   --enable-avformat
   --enable-avfilter

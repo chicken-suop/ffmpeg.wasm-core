9c5bcb3369fd84b2857062f591c2608f99a3bfef
Elliot Schep <elliot@desygner.com>
2022-11-20 18:20:49 +0000

    Fixes lavfi and anullsrc

--- wasm/build-scripts/configure-ffmpeg.sh
+++ wasm/build-scripts/configure-ffmpeg.sh
@@ -19,15 +19,16 @@ FLAGS=(
   --enable-avfilter
   --enable-swresample
   --enable-swscale
+  --enable-indev=lavfi
 
   --enable-decoder=aac,h264,mp3,mpeg4,rawvideo,mjpeg,png
   --enable-encoder=aac,h264,libx264,mpeg4
-  --enable-protocol=file,pipe
+  --enable-protocol=file,pipe # pipe is used by progress
   --enable-demuxer=aac,h264,mov,m4v,mp3,wav,image2
   --enable-muxer=mp4,h264
   --enable-parser=aac,h264,mpeg4video
   --enable-bsf=aac_adtstoasc
-  --enable-filter=fps,split,palettegen,fifo,paletteuse,scale,overlay,apad
+  --enable-filter=fps,split,palettegen,fifo,paletteuse,scale,overlay,apad,anullsrc
 )
 echo "FFMPEG_CONFIG_FLAGS=${FLAGS[@]}"
 

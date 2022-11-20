67ed7394abc6f9765cf62c144a7859a12bdd86b0
Elliot Schep <elliot@desygner.com>
2022-11-20 15:38:26 +0000

    Fix decoding png

--- wasm/build-scripts/configure-ffmpeg.sh
+++ wasm/build-scripts/configure-ffmpeg.sh
@@ -10,6 +10,7 @@ FLAGS=(
   --enable-gpl            # required by x264
   --enable-libx264        # enable x264
   --enable-libfdk-aac     # enable fdk-aac
+  --enable-zlib           # Needed for png decoding
 
   --enable-avdevice
   --enable-avutil
@@ -19,14 +20,14 @@ FLAGS=(
   --enable-swresample
   --enable-swscale
 
-  --enable-decoder=aac,aac_latm,h264,mp3,mpeg4,rawvideo
+  --enable-decoder=aac,h264,mp3,mpeg4,rawvideo,mjpeg,png
   --enable-encoder=aac,h264,libx264,mpeg4
   --enable-protocol=file
-  --enable-demuxer=aac,h264,mov,m4v,mp3,wav
+  --enable-demuxer=aac,h264,mov,m4v,mp3,wav,image2
   --enable-muxer=mp4,h264
   --enable-parser=aac,h264,mpeg4video
   --enable-bsf=aac_adtstoasc
-  --enable-filter=fps,split,palettegen,fifo,paletteuse,scale,overlay
+  --enable-filter=fps,split,palettegen,fifo,paletteuse,scale,overlay,apad
 )
 echo "FFMPEG_CONFIG_FLAGS=${FLAGS[@]}"
 
